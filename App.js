bc9ad12a8f3e82519d1e868e05a53935a273eebc
Ujjwal Gautam <39196584+ujm2099@users.noreply.github.com>
2021-04-28 22:35:31 +0530

    Added Navigation

--- App.js
+++ App.js
@@ -6,32 +6,36 @@ import Deck from './Screens/Deck';
 import NewDeck from './Screens/NewDeck';
 import NewCard from './Screens/NewCard';
 import ScoreCard from './Screens/ScoreCard';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 function App() {
   return (
-    <View>
-      {/*
-      <Card
-        title="Which company created ReactNative ?"
-        options={['Facebook', 'Google', 'Twitter', 'WhatsApp']}
-        answer={1}
-        cards={5}
-      />
-      */}
-      {
-        //<Home />
-      }
-      {
-        //<Deck title="ReactNative" cards={5} />
-      }
-      {
-        //<NewDeck />
-      }
-      {
-        //<NewCard />
-      }
-      <ScoreCard />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Deck"
+          component={Deck}
+          options={((title = 'ReactNative'), (cards = 5))}
+        />
+        <Stack.Screen
+          name="Card"
+          component={Card}
+          options={
+            ((title = 'Which company created ReactNative ?'),
+            (optio = ['Facebook', 'Google', 'Twitter', 'WhatsApp']),
+            (answer = 1),
+            (cards = 5))
+          }
+        />
+        <Stack.Screen name="NewCard" component={NewCard} />
+        <Stack.Screen name="NewDeck" component={NewDeck} />
+        <Stack.Screen name="ScoreCard" component={ScoreCard} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 const styles = StyleSheet.create({});
